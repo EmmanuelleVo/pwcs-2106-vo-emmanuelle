c9ae8445bcab9dbba68313f77c4714bbcb47195a
EmmanuelleVo <57753923+EmmanuelleVo@users.noreply.github.com>
2021-07-29 13:49:43 +0200

    Update Election.php

--- controllers/Election.php
+++ controllers/Election.php
@@ -12,31 +12,39 @@ class Election //Home Page
         $allCandidates = [];
         $candidates = $candidateModel->all();
         $view = './views/election.php';
-        $candidates = json_decode(json_encode($candidates), true);
+        //$candidates = json_decode(json_encode($candidates), true);
 
         // afficher les 2 premiers candidats selon l'arrondissement
 
+        $liste = [];
         foreach ($candidates as $candidate) {
             $candidateName = $candidate->nom;
             $candidateArrondissement = $candidate->arrondissement;
             $candidateLevel = $candidate->niveau;
             $candidateStatus = $candidate->statut;
 
-            $candidateName = $candidate['nom'];
-            $candidateArrondissement = $candidate['arrondissement'];
-            $candidateLevel = $candidate['niveau'];
-            $candidateStatus = $candidate['statut'];
+            if(isset($liste[$candidateArrondissement])) { // ps
+                var_dump('hi');
+                if(count($liste[$candidateArrondissement]) === 1 && ($candidateStatus === '1e de liste' || $candidateStatus === '2e de liste')) { // si 1 seul candidat,
+                    $liste = [
+                        ['arrondissement' => 'Belgique',
+                         'niveau' => 'Parlement européen'],
+                        ['ps' => 'Amandine', 'mail' => 'amandine@lp.fr'],
+                    ];
+                }
+            }else{
+                if(($candidateStatus === '1e de liste' || $candidateStatus === '2e de liste')) {
+                    $liste = ['ecolo' => 'Amandine', 'mail' => 'amandine@lp.fr'];
+                }
+            }
 
+            var_dump($candidateName);
+            die();
 
-            $allCandidates[$candidateName]['arrondissement'] = $candidateArrondissement;
-            $allCandidates[$candidateName]['niveau'] = $candidateLevel;
-            $allCandidates[$candidateName]['statut'] = $candidateStatus;
 
 
 
         }
-            //var_dump($allCandidates); die();
-
 
 
 
